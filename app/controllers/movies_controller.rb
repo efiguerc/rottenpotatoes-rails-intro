ec54c5e86988adbb5d0a1aec99993fb88e4e8953
Edgar Figueroa <efiguerc@gmail.com>
2017-09-24 01:05:52 +0000

    setup the rating checkboxes and refresh button

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = ['G','PG','PG-13','R']
     @sort = params[:sort]
     @movies = Movie.all.order(@sort)
   end
